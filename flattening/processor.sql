d8000499c1c6966a1c7e9b95800b626ba7493a9f
Bruno Campos <brunofdcampos@hotmail.com>
2022-12-14 10:43:37 +0000

    new jsons

---
+++ flattening/processor.sql
@@ -0,0 +1,7 @@
+"_AIRBYTE_DATA":"d_processor_id" AS D_PROCESSOR_ID
+, "_AIRBYTE_DATA":"etl_created_at" AS ETL_CREATED_AT
+, "_AIRBYTE_DATA":"etl_updated_at" AS ETL_UPDATED_AT
+, "_AIRBYTE_DATA":"processor_bk" AS PROCESSOR_BK
+, "_AIRBYTE_DATA":"processor_code" AS PROCESSOR_CODE
+, "_AIRBYTE_DATA":"processor_group" AS PROCESSOR_GROUP
+, "_AIRBYTE_DATA":"processor_name" AS PROCESSOR_NAME
